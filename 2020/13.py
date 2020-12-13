b8c184ac2bb6850f1f01ade7ed91bbe5ec67c902
Colin Siles <colin.siles16@gmail.com>
2020-12-13 10:23:17 -0700

    Clean up day 13 colution

--- 2020/13.py
+++ 2020/13.py
@@ -31,21 +31,19 @@ except Exception as e:
 # Part 1
 ans = 0
 
-start = int(lines[0])
+test = int(lines[0])
 nums = [int(line) for line in lines[1].split(',') if line != 'x']
 
 min_wait = 10000000
 min_num = 0
 for num in nums:
-    new = num - (start % num)
+    new = num - (test % num)
 
     if new < min_wait:
         min_wait = new
         min_num = num
 
-
 ans = min_wait * min_num
-
 
 print('Part 1:', ans)
 
@@ -56,27 +54,22 @@ print('Part 1:', ans)
 '''
 My solution to this part is based on the "Search by sieving method" described in this article:
 https://en.wikipedia.org/wiki/Chinese_remainder_theorem
-
-My original idea used a similar thought process, but began testing multiples of x2, instead of
-multiples of the previously solved factors
-
-I think sorting the factors by size wasn't necessary, because my code freezing had to do with
-negative target modulo values, but I think it still speeds it up
 '''
 
 ans = 0
 
+# Extract the bus numbers and their target departure offset
 nums = [int(num) for num in lines[1].split(',') if num != 'x']
 target_indices = [i for i, num in enumerate(lines[1].split(',')) if num != 'x']
 
-zipped = zip(nums, target_indices)
-zipped = list(zipped)
-zipped.sort()
-zipped = list(reversed(zipped))
+zipped = list(zip(nums, target_indices))
 
-nums = [elem[0] for elem in zipped]
+# Calculate the target modulus value to get the target departure offset
 mods = [elem[0] - elem[1] for elem in zipped]
 
+# Adjust some of the target mods
+# The first bus should have a modulus of 0
+# There should be no negative target modulus values
 for i, num in enumerate(nums):
     if mods[i] == num:
         mods[i] = 0
@@ -84,26 +77,21 @@ for i, num in enumerate(nums):
     while mods[i] < 0:
         mods[i] += nums[i]
 
+# Start testing at the lowest value that makes the first bus id work
 test = mods[0]
+
+# Increment by the first bus id so that its modulus is unchanged
 inc = nums[0]
 
-for i in range(2, len(nums) + 1):
-    while True:
-        valid = True
-
-        for j, num in enumerate(nums[:i]):
-            if test % num != mods[j]:
-                valid = False
-                break
-
-        if valid:
-            break
-
+# Incrementally solve each bus id after
+for i in range(1, len(nums)):
+    # Keep incrementing to find the lowest number that works for bus i
+    while test % nums[i] != mods[i]:
         test += inc
 
-    inc *= nums[i-1]
-    print('Solved i =', i)
-    print(test, inc)
+    # Once we solve this bus, multiple increment by the number, so future searches
+    # do not change the modulus of this bus
+    inc *= nums[i]
 
 ans = test
         
